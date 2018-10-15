1123553527d6db12d1f6f20bb7a4f1ef4a931ce7
zenghongqing <1543200280@qq.com>
2018-10-15 23:00:49 +0800

    代码优化

--- server/controller/index.js
+++ server/controller/index.js
@@ -48,28 +48,21 @@ const LoginUser = async (ctx, next) => {
     if (!params.username || !params.password) {
         return console.error('未填用户信息')
     }
-    let { data } = await Admin.findOne({
-        username: params.username,
-        password: md5(params.password)
-    }, {
-        __v: 0,
-        password: 0
-    }).then(res => {
-        let msg = ''
-        if (!res) {
-            msg = '用户信息不存在'
-        }
-        if (res.status !== 1) {
-            msg = '账号审核未通过'
-        }
-        return {
-            msg: msg || 'no',
-            data: res._id
-        }
-    })
-    ctx.body = data
-    ctx.status = 200
-    ctx.success = true
+    try {
+        let data = await Admin.findOne({
+            username: params.username,
+            password: md5(params.password)
+        }, {
+            __v: 0,
+            password: 0
+        })
+        ctx.body = data
+        ctx.status = 200
+        ctx.success = true
+    } catch (e) {
+        ctx.body = e
+        ctx.success = false
+    }
 }
 // 获取管理员信息
 const GetAdminInfo = async (ctx, next) => {
@@ -82,7 +75,6 @@ const GetAdminInfo = async (ctx, next) => {
         ctx.body = info
         ctx.status = 200
         ctx.success = true
-        next()
     } catch (e) {
         ctx.body = e
         ctx.success = false
@@ -151,16 +143,18 @@ const DeleteAdmin = async (ctx, next) => {
             _id: params.AdminToken
         })
         if (!adminList) {
-            ctx.body = {
-                msg: '用户信息不存在'
-            }
+            // ctx.body = {
+            //     msg: '用户信息不存在'
+            // }
+            return ctx.throw(404, {msg: '用户信息不存在'})
         } else {
             ctx.body = adminList
         }
     } else {
-        ctx.body = {
-            msg: adminInfo.message
-        }
+        // ctx.body = {
+        //     msg: adminInfo.message
+        // }
+        return ctx.throw(404, adminInfo.message)
     }
     ctx.status = 200
     ctx.success = true
